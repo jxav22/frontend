4afe9670b6a82c522afb2a6394bb2a4649156923
Jason <jasxavier7@gmail.com>
2023-11-05 02:09:23 +1300

    started doing a changelog experiment

---
+++ src/components/ChangeLog/ChangeLog.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ChangeLog() {
+  return (
+    <>
+        <h2>Change Log</h2>
+        <ul>
+            <li>Created the prototype!</li>
+        </ul>
+    </>
+  )
+}
+
+export default ChangeLog
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./LandingPage.module.css";
 import SocialButtons from "../SocialButtons/SocialButtons";
+import ChangeLog from "../ChangeLog/ChangeLog";
 
 function LandingPage({ onNextPage }: { onNextPage: () => void}) {
   return (
     <div className={styles.container}>
-      <div className={styles.leftPane} />
+      <div className={styles.leftPane}/>
       <SocialButtons/>
       <span className={styles.displacedArea} onClick={onNextPage}>
         <h1 className={styles.leftTitle}>Vent</h1>
